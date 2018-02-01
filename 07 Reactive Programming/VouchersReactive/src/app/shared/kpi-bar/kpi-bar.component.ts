fc54b92301e8962421e7200807aedba42644a1b6
Administrator@SPDOM.LOCAL <Administrator@SPDOM.LOCAL>
2018-02-01 15:53:53 +0100

    State Boilerplate added

---
+++ 07 Reactive Programming/VouchersReactive/src/app/shared/kpi-bar/kpi-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kpi-bar',
+  templateUrl: './kpi-bar.component.html',
+  styleUrls: ['./kpi-bar.component.scss']
+})
+export class KpiBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
