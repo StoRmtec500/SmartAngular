609e36712ff94288c1ef057821bc6c8ba85e821a
Alexander pajer <alexander.pajer@integrations.at>
2018-02-03 12:10:49 +0100

    Forms Validation changed to FB Reactive

--- 08 Forms & Validation/VouchersForms/src/app/demos/reative-validation/reative-validation.component.ts
+++ 08 Forms & Validation/VouchersForms/src/app/demos/reative-validation/reative-validation.component.ts
@@ -1,78 +1,78 @@
-import { Observable } from 'rxjs/Rx';
-import { Person } from '../../shared';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Rx";
+import { Person } from "../../shared";
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-reative-validation',
-  templateUrl: './reative-validation.component.html',
-  styleUrls: ['./reative-validation.component.css']
+  selector: "app-reative-validation",
+  templateUrl: "./reative-validation.component.html",
+  styleUrls: ["./reative-validation.component.css"]
 })
 export class ReativeValidationComponent implements OnInit {
-
   personForm: FormGroup;
-  personName: FormControl;
-  personAge: FormControl;
-  personGender: FormControl;
+  person: Person = {
+    name: "Heinz",
+    gender: "male",
+    age: 20,
+    mail: "derschoeneheinz@xyz.at"
+  };
 
-  person : Person = {name: "Heinz", gender: "male", age: 12, mail: "derschoeneheinz@xyz.at"};
-  
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-
-    this.personName = new FormControl(this.person.name, 
-      [Validators.required, 
-      Validators.minLength(4), 
-      this.validateNotHugo],     
-      this.validateNamesExist);
-    
-    this.personAge = new FormControl(this.person.age, 
-      Validators.min(120));
-    
-    this.personGender = new FormControl(this.person.gender);
-
-    this.personForm = new FormGroup({
-      pName: this.personName,
-      pAge: this.personAge,
-      pGender: this.personGender
-    })
+    this.personForm = this.fb.group({
+      name: [
+        this.person.name,
+        [
+          Validators.required,
+          Validators.minLength(4),
+          this.validateNotHugo,
+          this.validateNamesExist
+        ]
+      ],
+      age: [this.person.age, [Validators.min(18), Validators.min(99)]],
+      gender: [this.person.gender]
+    });
   }
 
-  savePerson(formValues){
-    console.log('saving person with values: ');
+  savePerson(formValues) {
+    console.log("saving person with values: ");
     console.log(formValues);
   }
 
-  validateName(){
-    return this.personName.valid || this.personName.untouched
+  validateName() {
+    return this.personForm.controls.name.valid ||
+      this.personForm.controls.name.untouched;
   }
 
-  validateNotHugo(control: FormControl): {[s: string]: boolean}{
-    if(control.value === "Hugo"){
-      return {'hugoNotAllowed': true}
+  validateNotHugo(control: FormControl): { [s: string]: boolean } {
+    if (control.value === "Hugo") {
+      return { hugoNotAllowed: true };
     }
     return null;
   }
 
-  validateNamesExist(control: FormControl): Promise<any> | Observable<any>{
-    
+  validateNamesExist(control: FormControl): Promise<any> | Observable<any> {
     //Mocking Http Call to service to check weather user exists
-    const result = new Promise<any>((resolve, reject)=>{
-      setTimeout(()=>{
-        if(control.value === "Alexander"){
-          resolve({'UserExists': true});
-        }
-        else{
+    const result = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === "Alexander") {
+          resolve({ UserExists: true });
+        } else {
           resolve(null);
         }
-      }, 1500)
-    })
+      }, 1500);
+    });
     return result;
   }
 
-  validateForm(form){
+  validateForm(form) {
     form.updateValueAndValidity();
-    form.controls['pName'].updateValueAndValidity();
+    form.controls["pName"].updateValueAndValidity();
   }
 }
