928f793c7ffe928facc31637abbab18f1a8345b3
Alexander pajer <alexander.pajer@integrations.at>
2018-02-04 10:46:34 +0100

    Layout changed to Flexbox

--- LabSolution/Vouchers/src/app/app.component.ts
+++ LabSolution/Vouchers/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UrlSegment } from '@angular/router/src/url_tree';
+import { RouterEvent, NavigationEnd } from '@angular/router';
 
 
 @Component({
@@ -6,6 +9,39 @@ import { Component, ViewEncapsulation } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+
+  isRoot: boolean;
+
+  constructor(private router: Router){
+    
+  }
+
+  ngOnInit() {  
+    this.subsRouteChange();
+   };
+
+  subsRouteChange(){
+    this.router.events.subscribe((evt:RouterEvent) => {
+      if(evt.url!=undefined){
+        this.isRoot = evt.url == "/";
+      }
+  })
+  }
+ 
+  showSidepanel(){
+    return this.isRoot ? "none" : "block";
+  }
+
+  setSideDivWidth(){
+    return this.isRoot ? 'flexSideHidden' : 'panel flexSide'
+  }
+
+  setSpacer(){
+    return this.isRoot ? '' : 'flexSpacer'
+  }
+
+  setMainDivWidth(){
+    return this.isRoot ? 'flexMainBig' : 'panel flexMain'
+  }
 }
